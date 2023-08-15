7e0c54129aa46cefefd559b5bd12ec5f733d147e
maylla04 <maylla.rie@gmail.com>
2023-08-15 16:11:11 -0400

    git handler

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,10 +1,38 @@
 'use server'
 
+const lista = [
+    {
+        name: 'Maylla',
+        email: 'maylla.nascimento@gmail.com',
+        password: 'maylla123',
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+},
+{
+    name: 'Maria',
+    email: 'maria.passos@gmail.com',
+    password: 'maria123',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+},
+{
+    name: 'Pablo',
+    email: 'pablo.passos@gmail.com',
+    password: 'pabla123',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+}
+]
 const getUserAuthenticated = (user) => {
+    let userAut = {
 
+    }
+      lista.map((usuario)=> {
+        if(user.email === usuario.email && user.password === usuario.password){
+            userAut = usuario
+        }
+      })
+      return userAut
 }
 
 const getUsers = () =>{
-        
+       return lista 
 }
 export { getUsers, getUserAuthenticated };
