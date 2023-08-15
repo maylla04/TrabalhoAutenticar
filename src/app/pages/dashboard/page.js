b01a0a7a827069f41d31d8a797329e2a4b5e3ccd
maylla04 <maylla.rie@gmail.com>
2023-08-15 16:11:41 -0400

    listar usuarios

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,11 +1,20 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
 
 export default async function Dashboard() {
-   
+   const lista = getUsers()
     return (
         <div>
             <h1>Dashboard</h1>
-            
+            {lista.map(lista => {
+                return(
+                 <div>
+                 <p>{lista.nome}</p>
+                 <p>{lista.email}</p>
+                 <p>{lista.password}</p>
+                 </div>
+                 )
+            })}
+           
         </div>
     );
 };
