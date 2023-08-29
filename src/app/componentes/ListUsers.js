da661f350fafb320aeb5c72ae9f3402cb7cd1eba
maylla04 <maylla.rie@gmail.com>
2023-08-29 15:43:13 -0400

    criando listUsers

--- src/app/componentes/ListUsers.js
+++ src/app/componentes/ListUsers.js
@@ -1,16 +1,15 @@
-import { Suspense } from "react"
 
 export default async function ListUsers({lista}) {
     await new Promise((resolve) => setTimeout(resolve, 3000))
     return(
         <div>
-            <Suspense fallback={<p>Carregando...</p>}>
-                {lista.map((lista, index) =>
+            
+                {lista?.map((lista, index) =>
                 <p key={index}>
                     {lista.name}
                 </p>
                 )}
-            </Suspense>
+           
         </div>
     )
 }
--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,19 +1,15 @@
+import ListUsers from "@/app/componentes/ListUsers";
 import { getUsers } from "@/app/functions/handlerAcessAPI";
+import { Suspense } from "react"
 
 export default async function Dashboard() {
    const lista = getUsers()
     return (
         <div>
             <h1>Dashboard</h1>
-            {lista?.map((lista, index) => {
-                return(
-                 <div key={index}>
-                 <p>{lista.nome}</p>
-                 <p>{lista.email}</p>
-                 <p>{lista.password}</p>
-                 </div>
-                 )
-            })}
+            <Suspense fallback={<p>Carregando</p>}>
+                <ListUsers lista={lista}/>
+            </Suspense>
            
         </div>
     );
