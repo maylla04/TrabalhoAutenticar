e9453b980909f11d93d3c565ddc9147217a1b3ef
maylla04 <maylla.rie@gmail.com>
2023-09-05 15:41:31 -0300

    git nav

--- src/app/pages/dashboard/layout.js
+++ src/app/pages/dashboard/layout.js
@@ -19,11 +19,19 @@ export default function DashboardLayout({ children }){
     return(
         <body>
             <div>
-                <h1>Nav</h1>
-                <Link href={"/pages/dashboard"}>Home</Link>
-                <Link href={"/pages/dashboard/alter"}>Alterar</Link>
-                <Link href={"/pages/dashboard/register"}>Registrar</Link>
+            <nav class="flex items-center justify-between bg-gray-900 text-white p-4">
+  <div class="flex items-center">
+    <a href="#" class="text-xl font-bold">Logo</a>
+  </div>
+  <div class="flex">
+  <Link href={"/pages/dashboard"} className="mr-4">Home</Link>
+                <Link href={"/pages/dashboard/alter" } className="mr-4">Alterar</Link>
+                <Link href={"/pages/dashboard/register"} className="mr-4">Registrar</Link>
                 <button onClick={logout}>Sair</button>
+  </div>
+</nav>
+
+                
                 <div>{children}</div>
             </div>
         </body>
