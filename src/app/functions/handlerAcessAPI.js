11eea71d4f6163142276839325a26c0fff4c8e5a
maylla04 <maylla.rie@gmail.com>
2023-10-17 16:09:15 -0400

    git autenthicated

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,8 +1,16 @@
 'use server'
 const url = "https://lado-servidor-ptac-4.vercel.app";
 
-const getUserAuthenticated = (user) => { //usuario authenticado
-   
+const getUserAuthenticated = async (user) => { //usuario authenticado
+   const responseOfApi = await fetch(url + "/user/authenticated",
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(user)
+    }
+   );
+   const userAuth = await responseOfApi.json();
+   return userAuth;
 }
 
 const getUsers = () =>{
