e1be8420db6a3fe9f82b1646c580c341f837cdcf
maylla04 <maylla.rie@gmail.com>
2023-10-17 15:42:23 -0400

    git url server

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,49 +1,11 @@
 'use server'
+const url = "https://lado-servidor-ptac-4.vercel.app";
 
-const lista = [
-    {
-        name: 'Maylla',
-        email: 'maylla.passos@gmail.com',
-        password: 'maylla123',
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-},
-{
-    name: 'Maria',
-    email: 'maria.passos@gmail.com',
-    password: 'maria123',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-},
-{
-    name: 'Pablo',
-    email: 'pablo.passos@gmail.com',
-    password: 'pablo123',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-},
-{
-    name: 'Marcelino',
-    email: 'marcelino.passos@gmail.com',
-    password: 'marcelino123',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-},
-{
-    name: 'Renatao',
-    email: 'renato.passos@gmail.com',
-    password: 'renato123',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-}
-]
 const getUserAuthenticated = (user) => { //usuario authenticado
-    let userAut = {}
-      lista.map((usuario)=> {
-        if(user.email === usuario.email && user.password === usuario.password){
-            //se o email e a senha digitada forem iguais a alguma da lista
-            userAut = usuario
-        }
-      })
-      return userAut
+   
 }
 
 const getUsers = () =>{
-       return lista //retorna usuarios
+    
 }
 export { getUsers, getUserAuthenticated };
